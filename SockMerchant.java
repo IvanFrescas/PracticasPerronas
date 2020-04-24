6843fad347cb3464adc1c14845d8597517c4a5a1
Ivan Frescas <ivanfrescas8@gmail.com>
2020-04-23 22:10:26 -0600

    Remove tests

--- SockMerchant.java
+++ SockMerchant.java
@@ -9,17 +9,11 @@ import static org.junit.Assert.assertNotEquals;
 public class Main {
 
     public static void main(String[] args) {
-
-        int n = 9;
-        int[] ar = {10, 20, 20, 10, 10, 30, 50, 10, 20};
-        int[] ar2 = {1,2,1,2,1,3,2};
-
-
-
+        
     }
 
     static int sockMerchant(int n, int[] ar) {
-
+        int result = 0;
         HashMap<Integer, Integer> socksMap = new HashMap<>();
 
         for (int i : ar) {
@@ -30,24 +24,10 @@ public class Main {
             }
         }
 
-        int result = 0;
-
         for (Integer sock : socksMap.keySet()) {
             result += socksMap.get(sock) / 2;
         }
 
         return result;
     }
-
-    @org.junit.Test
-    public void sockMerchantTest() {
-        assertEquals(3, sockMerchant(9, new int[] {10, 20, 20, 10, 10, 30, 50, 10, 20}));
-    }
-
-    @org.junit.Test
-    public void sockMerchantTestNotFunctional() {
-        assertNotEquals(10, sockMerchant(9, new int[] {10, 20, 20, 10, 10, 30, 50, 10, 20}));
-    }
-
-
 }
