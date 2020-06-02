7e73927025f88699c6286b2b2e74552822b3d99b
José Francisco Castillo Rodríguez <josefrancasrod@gmail.com>
2020-06-01 21:42:24 -0500

    Add Bon Appetite

---
+++ bonappetite.js
@@ -0,0 +1,19 @@
+var bill= [3, 10, 2, 9]
+var k = 1;
+var b = 7;
+
+function bonAppetite(bill, k, b){
+
+    var total = 0;
+    var splitAccount = 0;
+    bill.map((item) => {
+        return total += item;
+    });
+    splitAccount = (total - bill[k])/2;
+    if(b == splitAccount){
+        console.log('Bon Appetit');
+    }else{
+        console.log(b-splitAccount);
+    }
+}
+bonAppetite(bill, k, b);
